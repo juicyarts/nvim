3774972dfdc71a8747456e3666ccb1c6a0375489
huess <huess@juicyarts.de>
2024-01-04 21:04:43 +0100

    chore(comments): allow easy commenting of code blocks

--- init.lua
+++ init.lua
@@ -27,6 +27,7 @@ vim.opt.rtp:prepend(lazypath)
 --    as they will be available in your neovim runtime.
 require('lazy').setup({                        -- NOTE: First, some plugins that don't require any configuration
   -- Git related plugins
+  'tpope/vim-commentary',
   'tpope/vim-fugitive', 'tpope/vim-rhubarb', -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',                        -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
