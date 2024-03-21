57e4e3f82cbff25c3bdc308bc7b2d8415d4e5d2c
huess <huess@juicyarts.de>
2024-03-21 14:54:13 +0100

    chore(cleanup): some cleanups and removal of unused stuff

--- init.lua
+++ init.lua
@@ -10,8 +10,8 @@ vim.g.maplocalleader = ' '
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn
-  .system { 'git', 'clone', '--filter=blob:none', 'https://github.com/folke/lazy.nvim.git', '--branch=stable', -- latest stable release
-    lazypath }
+      .system { 'git', 'clone', '--filter=blob:none', 'https://github.com/folke/lazy.nvim.git', '--branch=stable', -- latest stable release
+        lazypath }
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -25,7 +25,7 @@ vim.opt.rtp:prepend(lazypath)
 --
 --  You can also configure plugins after the setup call,
 --    as they will be available in your neovim runtime.
-require('lazy').setup({                        -- NOTE: First, some plugins that don't require any configuration
+require('lazy').setup({ -- NOTE: First, some plugins that don't require any configuration
   -- Git related plugins
   'tpope/vim-commentary',
   'tpope/vim-fugitive', 'tpope/vim-rhubarb', -- Detect tabstop and shiftwidth automatically
@@ -46,13 +46,13 @@ require('lazy').setup({                        -- NOTE: First, some plugins that
       }, -- Additional lua configuration, makes nvim stuff amazing!
       'folke/neodev.nvim' }
   }, {
-    -- Autocompletion
-    'hrsh7th/nvim-cmp',
-    dependencies = {                                  -- Snippet Engine & its associated nvim-cmp source
-      'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip', -- Adds LSP completion capabilities
-      'hrsh7th/cmp-nvim-lsp', 'hrsh7th/cmp-path',     -- Adds a number of user-friendly snippets
-      'rafamadriz/friendly-snippets' }
-  },                                                  -- Useful plugin to show you pending keybinds.
+  -- Autocompletion
+  'hrsh7th/nvim-cmp',
+  dependencies = {                                  -- Snippet Engine & its associated nvim-cmp source
+    'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip', -- Adds LSP completion capabilities
+    'hrsh7th/cmp-nvim-lsp', 'hrsh7th/cmp-path',     -- Adds a number of user-friendly snippets
+    'rafamadriz/friendly-snippets' }
+},                                                  -- Useful plugin to show you pending keybinds.
   {
     'folke/which-key.nvim',
     opts = {}
@@ -68,151 +68,151 @@ require('lazy').setup({                        -- NOTE: First, some plugins that
       }
     end
   }, {
-    "folke/zen-mode.nvim",
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    }
-  }, {
-    -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      -- See `:help gitsigns.txt`
-      signs = {
-        add = {
-          text = '+'
-        },
-        change = {
-          text = '~'
-        },
-        delete = {
-          text = '_'
-        },
-        topdelete = {
-          text = '‾'
-        },
-        changedelete = {
-          text = '~'
-        }
+  "folke/zen-mode.nvim",
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  }
+}, {
+  -- Adds git related signs to the gutter, as well as utilities for managing changes
+  'lewis6991/gitsigns.nvim',
+  opts = {
+    -- See `:help gitsigns.txt`
+    signs = {
+      add = {
+        text = '+'
       },
-      on_attach = function(bufnr)
-        local gs = package.loaded.gitsigns
-
-        local function map(mode, l, r, opts)
-          opts = opts or {}
-          opts.buffer = bufnr
-          vim.keymap.set(mode, l, r, opts)
-        end
-
-        -- Navigation
-        map({ 'n', 'v' }, ']c', function()
-          if vim.wo.diff then
-            return ']c'
-          end
-          vim.schedule(function()
-            gs.next_hunk()
-          end)
-          return '<Ignore>'
-        end, {
-            expr = true,
-            desc = 'Jump to next hunk'
-          })
-
-        map({ 'n', 'v' }, '[c', function()
-          if vim.wo.diff then
-            return '[c'
-          end
-          vim.schedule(function()
-            gs.prev_hunk()
-          end)
-          return '<Ignore>'
-        end, {
-            expr = true,
-            desc = 'Jump to previous hunk'
-          })
-
-        -- Actions
-        -- visual mode
-        map('v', '<leader>hs', function()
-          gs.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
-        end, {
-            desc = 'stage git hunk'
-          })
-        map('v', '<leader>hr', function()
-          gs.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
-        end, {
-            desc = 'reset git hunk'
-          })
-        -- normal mode
-        map('n', '<leader>hs', gs.stage_hunk, {
-          desc = 'git stage hunk'
-        })
-        map('n', '<leader>hr', gs.reset_hunk, {
-          desc = 'git reset hunk'
-        })
-        map('n', '<leader>hS', gs.stage_buffer, {
-          desc = 'git Stage buffer'
-        })
-        map('n', '<leader>hu', gs.undo_stage_hunk, {
-          desc = 'undo stage hunk'
-        })
-        map('n', '<leader>hR', gs.reset_buffer, {
-          desc = 'git Reset buffer'
-        })
-        map('n', '<leader>hp', gs.preview_hunk, {
-          desc = 'preview git hunk'
-        })
-        map('n', '<leader>hb', function()
-          gs.blame_line {
-            full = false
-          }
-        end, {
-            desc = 'git blame line'
-          })
-        map('n', '<leader>hd', gs.diffthis, {
-          desc = 'git diff against index'
-        })
-        map('n', '<leader>hD', function()
-          gs.diffthis '~'
-        end, {
-            desc = 'git diff against last commit'
-          })
-
-        -- Toggles
-        map('n', '<leader>tb', gs.toggle_current_line_blame, {
-          desc = 'toggle git blame line'
-        })
-        map('n', '<leader>td', gs.toggle_deleted, {
-          desc = 'toggle git show deleted'
-        })
-
-        -- Text object
-        map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', {
-          desc = 'select git hunk'
-        })
-      end
-    }
-  }, {
-    -- Set lualine as statusline
-    'nvim-lualine/lualine.nvim',
-    -- See `:help lualine.txt`
-    opts = {
-      options = {
-        icons_enabled = false,
-        theme = 'dracula-nvim',
-        component_separators = '|',
-        section_separators = ''
+      change = {
+        text = '~'
+      },
+      delete = {
+        text = '_'
+      },
+      topdelete = {
+        text = '‾'
+      },
+      changedelete = {
+        text = '~'
       }
+    },
+    on_attach = function(bufnr)
+      local gs = package.loaded.gitsigns
+
+      local function map(mode, l, r, opts)
+        opts = opts or {}
+        opts.buffer = bufnr
+        vim.keymap.set(mode, l, r, opts)
+      end
+
+      -- Navigation
+      map({ 'n', 'v' }, ']c', function()
+        if vim.wo.diff then
+          return ']c'
+        end
+        vim.schedule(function()
+          gs.next_hunk()
+        end)
+        return '<Ignore>'
+      end, {
+        expr = true,
+        desc = 'Jump to next hunk'
+      })
+
+      map({ 'n', 'v' }, '[c', function()
+        if vim.wo.diff then
+          return '[c'
+        end
+        vim.schedule(function()
+          gs.prev_hunk()
+        end)
+        return '<Ignore>'
+      end, {
+        expr = true,
+        desc = 'Jump to previous hunk'
+      })
+
+      -- Actions
+      -- visual mode
+      map('v', '<leader>hs', function()
+        gs.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
+      end, {
+        desc = 'stage git hunk'
+      })
+      map('v', '<leader>hr', function()
+        gs.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
+      end, {
+        desc = 'reset git hunk'
+      })
+      -- normal mode
+      map('n', '<leader>hs', gs.stage_hunk, {
+        desc = 'git stage hunk'
+      })
+      map('n', '<leader>hr', gs.reset_hunk, {
+        desc = 'git reset hunk'
+      })
+      map('n', '<leader>hS', gs.stage_buffer, {
+        desc = 'git Stage buffer'
+      })
+      map('n', '<leader>hu', gs.undo_stage_hunk, {
+        desc = 'undo stage hunk'
+      })
+      map('n', '<leader>hR', gs.reset_buffer, {
+        desc = 'git Reset buffer'
+      })
+      map('n', '<leader>hp', gs.preview_hunk, {
+        desc = 'preview git hunk'
+      })
+      map('n', '<leader>hb', function()
+        gs.blame_line {
+          full = false
+        }
+      end, {
+        desc = 'git blame line'
+      })
+      map('n', '<leader>hd', gs.diffthis, {
+        desc = 'git diff against index'
+      })
+      map('n', '<leader>hD', function()
+        gs.diffthis '~'
+      end, {
+        desc = 'git diff against last commit'
+      })
+
+      -- Toggles
+      map('n', '<leader>tb', gs.toggle_current_line_blame, {
+        desc = 'toggle git blame line'
+      })
+      map('n', '<leader>td', gs.toggle_deleted, {
+        desc = 'toggle git show deleted'
+      })
+
+      -- Text object
+      map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', {
+        desc = 'select git hunk'
+      })
+    end
+  }
+}, {
+  -- Set lualine as statusline
+  'nvim-lualine/lualine.nvim',
+  -- See `:help lualine.txt`
+  opts = {
+    options = {
+      icons_enabled = false,
+      theme = 'dracula-nvim',
+      component_separators = '|',
+      section_separators = ''
     }
-  }, {
-    -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
-    main = 'ibl',
-    opts = {}
-  }, -- "gc" to comment visual regions/lines
+  }
+}, {
+  -- Add indentation guides even on blank lines
+  'lukas-reineke/indent-blankline.nvim',
+  -- Enable `lukas-reineke/indent-blankline.nvim`
+  -- See `:help ibl`
+  main = 'ibl',
+  opts = {}
+}, -- "gc" to comment visual regions/lines
   {
     'numToStr/Comment.nvim',
     opts = {}
@@ -241,50 +241,58 @@ require('lazy').setup({                        -- NOTE: First, some plugins that
       require("telescope").load_extension("live_grep_args")
     end
   }, {
-    -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
-    dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
-    build = ':TSUpdate'
-  }, {
-    -- theme
-    'Mofiqul/dracula.nvim',
-    lazy = false,
-    priority = 1000,
-    opts = {},
-    config = function()
-      vim.cmd.colorscheme 'dracula'
-    end
-  }, {
-    "kdheepak/lazygit.nvim",
-    -- optional for floating window border decoration
-    dependencies = {
-      "nvim-telescope/telescope.nvim",
-      "nvim-lua/plenary.nvim",
-    },
-    config = function()
-      require("telescope").load_extension("lazygit")
-    end,
-  }, {
-    -- games
-    'ThePrimeagen/vim-be-good',
-  }, {
-    'nvim-lua/plenary.nvim',
-  }, {
-    -- fast switch buffer
-    'ThePrimeagen/harpoon',
+  -- Highlight, edit, and navigate code
+  'nvim-treesitter/nvim-treesitter',
+  dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
+  build = ':TSUpdate'
+}, {
+  -- theme
+  'Mofiqul/dracula.nvim',
+  lazy = false,
+  priority = 1000,
+  opts = {},
+  config = function()
+    vim.cmd.colorscheme 'dracula'
+  end
+}, {
+  "kdheepak/lazygit.nvim",
+  -- optional for floating window border decoration
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "nvim-lua/plenary.nvim",
   },
-  {
-    -- git without git
-    'mbbill/undotree',
-  }, {
-    'wuelnerdotexe/vim-astro'
-  }, {
-    'github/copilot.vim'
+  config = function()
+    require("telescope").load_extension("lazygit")
+  end,
+}, {
+  -- games
+  'ThePrimeagen/vim-be-good',
+}, {
+  'nvim-lua/plenary.nvim',
+}, {
+  -- fast switch buffer
+  'ThePrimeagen/harpoon',
+}, {
+  -- git without git
+  'mbbill/undotree',
+}, {
+  'wuelnerdotexe/vim-astro'
+}, {
+  import = 'custom.plugins'
+}, {
+  "ray-x/go.nvim",
+  dependencies = { -- optional packages
+    "ray-x/guihua.lua",
+    "neovim/nvim-lspconfig",
+    "nvim-treesitter/nvim-treesitter",
   },
-  {
-    import = 'custom.plugins'
-  } },
-  {})
+  config = function()
+    require("go").setup()
+  end,
+  event = { "CmdlineEnter" },
+  ft = { "go", 'gomod' },
+  build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+}})
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
@@ -485,8 +493,8 @@ vim.keymap.set('n', '<leader>/', function()
     previewer = false
   })
 end, {
-    desc = '[/] Fuzzily search in current buffer'
-  })
+  desc = '[/] Fuzzily search in current buffer'
+})
 
 local function telescope_live_grep_open_files()
   require('telescope.builtin').live_grep {
@@ -651,8 +659,8 @@ local on_attach = function(_, bufnr)
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
     vim.lsp.buf.format()
   end, {
-      desc = 'Format current buffer with LSP'
-    })
+    desc = 'Format current buffer with LSP'
+  })
 end
 
 -- document existing key chains
@@ -698,8 +706,8 @@ require('which-key').register({
   },
   ['<leader>h'] = { 'Git [H]unk' }
 }, {
-    mode = 'v'
-  })
+  mode = 'v'
+})
 
 -- mason-lspconfig requires that these setup functions are called in this order
 -- before setting up the servers.
@@ -716,7 +724,7 @@ require('mason-lspconfig').setup()
 --  define the property 'filetypes' to the map in question.
 local servers = {
   -- clangd = {},
-  -- gopls = {},
+  gopls = {},
   -- pyright = {},
   -- rust_analyzer = {},
   -- tsserver = {},
@@ -735,6 +743,8 @@ local servers = {
     }
   }
 }
+
+-- vim.filetype.add({ extension = { templ = "templ" } })
 
 -- Setup neovim lua configuration
 require('neodev').setup()
@@ -807,10 +817,10 @@ cmp.setup {
   sources = { {
     name = 'nvim_lsp'
   }, {
-      name = 'luasnip'
-    }, {
-      name = 'path'
-    } }
+    name = 'luasnip'
+  }, {
+    name = 'path'
+  } }
 }
 
 
@@ -1102,6 +1112,9 @@ vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
     border = "single"
   }
 )
+
+-- [[ GO ]]
+require('go').setup()
 
 --vim.diagnostic.config{
 --  float={border = "single"}
