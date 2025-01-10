bc3d9bee30d748c87d0a3feb4076d58cece328b6
huess <huess@juicyarts.de>
2025-01-10 18:55:37 +0100

    chore(deps): housekeeping, updates and migrations

--- init.lua
+++ init.lua
@@ -701,47 +701,21 @@ local on_attach = function(_, bufnr)
 end
 
 -- document existing key chains
-require('which-key').register {
-  ['<leader>c'] = {
-    name = '[C]ode',
-    _ = 'which_key_ignore'
-  },
-  ['<leader>d'] = {
-    name = '[D]ocument',
-    _ = 'which_key_ignore'
-  },
-  ['<leader>g'] = {
-    name = '[G]it',
-    _ = 'which_key_ignore'
-  },
-  ['<leader>h'] = {
-    name = 'Git [H]unk',
-    _ = 'which_key_ignore'
-  },
-  ['<leader>r'] = {
-    name = '[R]ename',
-    _ = 'which_key_ignore'
-  },
-  ['<leader>s'] = {
-    name = '[S]earch',
-    _ = 'which_key_ignore'
-  },
-  ['<leader>t'] = {
-    name = '[T]oggle',
-    _ = 'which_key_ignore'
-  },
-  ['<leader>w'] = {
-    name = '[W]orkspace',
-    _ = 'which_key_ignore'
-  }
+require('which-key').add {
+  { '<leader>c', group = '[C]ode' },
+  { '<leader>d', group = '[D]ocument' },
+  { '<leader>g', group = '[G]it' },
+  { '<leader>h', group = 'Git [H]unk' },
+  { '<leader>r', group = '[R]ename' },
+  { '<leader>s', group = '[S]earch' },
+  { '<leader>t', group = '[T]oggle' },
+  { '<leader>w', group = '[W]orkspace' },
 }
 -- register which-key VISUAL mode
 -- required for visual <leader>hs (hunk stage) to work
-require('which-key').register({
-  ['<leader>'] = {
-    name = 'VISUAL <leader>'
-  },
-  ['<leader>h'] = { 'Git [H]unk' }
+require('which-key').add({
+  { '<leader>',  group = 'VISUAL <leader>' },
+  { '<leader>h', group = 'Git [H]unk' },
 }, {
   mode = 'v'
 })
@@ -774,9 +748,8 @@ local servers = {
       },
       telemetry = {
         enable = false
-      }
-      -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-      -- diagnostics = { disable = { 'missing-fields' } },
+      },
+      diagnostics = { disable = { 'missing-fields' } },
     }
   }
 }
@@ -796,11 +769,11 @@ require('lspconfig').html.setup({
   filetypes = { "html", "templ" },
 })
 
-require('lspconfig').htmx.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  filetypes = { "html", "templ" },
-})
+-- require('lspconfig').htmx.setup({
+--   on_attach = on_attach,
+--   capabilities = capabilities,
+--   filetypes = { "html", "templ" },
+-- })
 
 require('lspconfig').ols.setup({})
 
